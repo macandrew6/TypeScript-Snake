3eeb7a4ddaed8b3e3d44ea5ffd73ae5ad09e0580
macandrew6 <mr.betterandrewmac@gmail.com>
2019-09-12 18:42:57 -0700

    starts variable types tests

--- TYPESCRIPT/main.js
+++ TYPESCRIPT/main.js
@@ -2,4 +2,10 @@
 exports.__esModule = true;
 var message = "Welcome back";
 console.log(message);
-//variable declaration:
+// => Variable Declaration:
+var x = 10;
+var y = 20;
+var sum = 30;
+//const must always be initiated with a value
+var title = 'epic';
+// => Variable Types:
--- TYPESCRIPT/main.ts
+++ TYPESCRIPT/main.ts
@@ -2,4 +2,12 @@ export {}
 let message = "Welcome back";
 console.log(message);
 
-//variable declaration:
+// => Variable Declaration:
+let x = 10;
+const y = 20;
+
+let sum = 30;
+//const must always be initiated with a value
+const title = 'epic';
+
+// => Variable Types:
