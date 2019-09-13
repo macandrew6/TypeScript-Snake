5fd3d6397e6233fccfdd3f6209a9e24cc01b2bc0
macandrew6 <mr.betterandrewmac@gmail.com>
2019-09-12 22:00:52 -0700

    more types

--- TYPESCRIPT/main.js
+++ TYPESCRIPT/main.js
@@ -8,4 +8,28 @@ var y = 20;
 var sum = 30;
 //const must always be initiated with a value
 var title = 'epic';
-// => Variable Types:
+// => Variable Types: pros: (static type checking, intellisense)
+//basic types
+var isBeginner = true;
+var total = 0;
+var name = 'andrew';
+var sentence = "My name is " + name + " I am a beginner in Typescript";
+console.log(sentence);
+//subtypes
+var u = undefined;
+var n = null;
+var isNew = null;
+var myName = undefined;
+//arrays
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+var person1 = ['Greg', 88];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 5] = "Red";
+    Color[Color["Green"] = 6] = "Green";
+    Color[Color["Blue"] = 7] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Green;
+console.log(c);
--- TYPESCRIPT/main.ts
+++ TYPESCRIPT/main.ts
@@ -10,4 +10,30 @@ let sum = 30;
 //const must always be initiated with a value
 const title = 'epic';
 
-// => Variable Types:
+// => Variable Types: pros: (static type checking, intellisense)
+//basic types
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'andrew';
+let sentence: string = `My name is ${name} I am a beginner in Typescript`;
+
+console.log(sentence);
+
+//subtypes
+let u: undefined = undefined;
+let n: null = null;
+
+let isNew: boolean = null;
+let myName: string = undefined;
+
+//arrays
+let list1: number[] = [ 1, 2, 3 ]
+let list2: Array<number> = [ 1, 2, 3 ]
+
+let person1: [string, number] = ['Greg', 88]
+
+enum Color {Red = 5, Green, Blue};
+
+let c: Color = Color.Green
+
+console.log(c);
